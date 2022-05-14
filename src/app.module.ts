5a115fff201efe225f605bbc20de6470a14e4815
Osid Abu-Alrub <o.Abu-Alrub@foothillsolutions.com>
2022-05-15 02:53:46 +0300

    import user and post modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
-import { ProductModule } from "./product/product.module"
 import { PrismaModule } from "src/prisma/prisma.module"
+import { PostModule } from "./post/post.module"
+import { UserModule } from "./user/user.module"
 
 // this is the entry point
 // if you dont import modules here, you will not be able
 // to use them in your application
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule, ProductModule],
+  imports: [ConfigModule.forRoot(), PrismaModule, PostModule, UserModule],
 })
 export class AppModule {}
